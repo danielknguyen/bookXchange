420873bbeff0e8da251fe94ae81384dc4eb2bd45
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-04-29 21:20:32 -0700

    sidebar navigation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-
+  /* Set the width of the side navigation to 250px */
+  function openNav() {
+      document.getElementById("mySidenav").style.width = "100%";
+  };
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+  };
+  $('.sideNavButton').on('click', function() {
+    openNav();
+  });
+  $('.closebtn').on('click', function() {
+    closeNav();
+  });
 });
