496fab21014c93773750cf4c831d06bba761fd57
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-05-25 12:07:16 -0700

    added requestSchema :

---
+++ models/requestSchema.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose'),
+    // mongoose schema method
+    Schema = mongoose.Schema,
+    // mongoose objectId
+    ObjectId = Schema.ObjectId,
+    bcrypt = require('bcrypt-nodejs');
+
+var db = require('../libs/db.js');
+
+var requestSchema = new Schema({
+  id: ObjectId,
+  user_id: {
+    type: String,
+    trim: true
+  },
+  book_id: {
+    type: String,
+    trim: true
+  },
+  title: {
+    type: String,
+    trim: true
+  },
+  ownerId: {
+    type: String,
+    trim: true
+  },
+  owner: {
+    type: String,
+    trim: true
+  },
+  book_id_to_give: {
+    type: String,
+    trim: true
+  },
+  title_to_give: {
+    type: String,
+    trim: true
+  },
+  status: {
+    type: String,
+    trim: true
+  }
+});
+
+var Request = db.model('Request', requestSchema);
+module.exports = Request;
